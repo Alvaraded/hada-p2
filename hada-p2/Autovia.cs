8baaf34bbbd29069a4f8e2a5243726eaadb981be
Alvaraded <32101099+Alvaraded@users.noreply.github.com>
2019-03-10 18:30:19 +0100

    clasesd

---
+++ hada-p2/Autovia.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Hada
+{
+    class Autovia
+    {
+        //ArrayList<Vehiculo> coches = new ArrayList<Vehiculo>();
+        List<Vehiculo> coches = new List<Vehiculo>();
+
+        public Autovia(int nc)
+        {
+            Vehiculo aux;
+            string auxs = "coche_";
+            for (int i = 0; i < nc; i++)
+            {
+                auxs += i;
+                aux = new Vehiculo(auxs, 50, 50, 50);
+                coches.Add(aux);
+                auxs = "coche_";
+            }
+        }
+
+        public bool moverCoches()
+        {
+            bool mv = false;
+            for (int i = 0; i < coches.Count; i++)
+            {
+                coches[i].mover();
+                if (coches[i].todoOk())
+                {
+                    mv = true;
+                }
+            }
+            return mv;
+        }
+
+        public void moverCochesEnBucle()
+        {
+            bool it = false;
+            do {
+                it = moverCoches();
+            } while (it);
+        }
+
+        public List<Vehiculo> getCochesExcedenLimiteVelocidad()
+        {
+            List<Vehiculo> ev = new List<Vehiculo>();
+            for (int i = 0; i < coches.Count; i++)
+            {
+                if (coches[i].velocidad > Vehiculo.maxVelocidad)
+                {
+                    ev.Add(coches[i]);
+                }
+            }
+            return ev;
+        }
+
+        public List<Vehiculo> getCochesExcedenLimiteTemperatura()
+        {
+            List<Vehiculo> et = new List<Vehiculo>();
+            for (int i = 0; i < coches.Count; i++)
+            {
+                if (coches[i].temperatura > Vehiculo.maxTemperatura)
+                {
+                    et.Add(coches[i]);
+                }
+            }
+            return et;
+        }
+
+        public List<Vehiculo> getCochesExcedenMinimoCombustible()
+        {
+            List<Vehiculo> dc = new List<Vehiculo>();
+            for (int i = 0; i < coches.Count; i++)
+            {
+                if (coches[i].combustible < Vehiculo.minCombustible)
+                {
+                    dc.Add(coches[i]);
+                }
+            }
+            return dc;
+        }
+
+        public override string ToString()
+        {
+            // List aux = new List();
+            string str = "";
+            List<Vehiculo> ev = new List<Vehiculo>();
+            ev = getCochesExcedenLimiteVelocidad();
+            int cev = ev.Count();
+            List<Vehiculo> et = new List<Vehiculo>();
+            et = getCochesExcedenLimiteTemperatura();
+            int cet = et.Count();
+            List<Vehiculo> dc = new List<Vehiculo>();
+            dc = getCochesExcedenMinimoCombustible();
+            int cdc = dc.Count();
+            str = "[AUTOVÍA] Exceso velocidad:" + cev + "; Exceso temperatura: " + cet + ": Déficit combustible: " + cdc + "\n";
+            for (int i = 0; i < coches.Count; i++)
+            {
+                coches[i].ToString();
+            }
+            return str;
+        }
+
+        private void cuandoVelocidadMaximaExcedida(Vehiculo c)
+        {
+            Console.WriteLine("!!Velocidad máxima excedida!!\n");
+            Console.WriteLine("Vehículo: " + c.nombre + "\n");
+            Console.WriteLine("Velocidad: " + c.velocidad);
+
+
+        }
+
+        private void cuandoTemperaturaMaximaExcedida()
+        {
+
+        }
+
+        private void cuandoCombustibleMinimoExcedido()
+        {
+
+        }
+    }
+}
---
+++ hada-p2/Vehiculo.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    class Vehiculo
+    {
+        public static int maxVelocidad
+        {
+            set;
+            get;
+        }
+        public static int maxTemperatura
+        {
+            set;
+            get;
+        }
+        public static int minCombustible
+        {
+            set;
+            get;
+        }
+        public static Random rand
+        {
+            private get; set;
+        }
+
+        private int _velocidad;
+        private int _temperatura;
+        private int _combustible;
+
+        public string nombre
+        {
+            get; private set;
+        }
+        public Vehiculo(string nombre, int velocidad, int temperatura, int combustible)
+        {
+            this.nombre = nombre;
+            _velocidad = velocidad;
+            _temperatura = temperatura;
+            _combustible = combustible;
+        }
+
+        public int temperatura
+        {
+            set
+            {
+                if (value > maxTemperatura)
+                {
+                //    temperaturaMaximaExcedida(this, new TemperaturaMaximaExcedidaArgs(value));
+                }
+                else
+                {
+                    _temperatura = value;
+                }
+            }
+            get
+            {
+                return _temperatura;
+            }
+        }
+
+        public int combustible
+        {
+            set
+            {
+                if (value <= minCombustible)
+                {
+                  //  combustibleMinimoExcedido(this, new CombustibleMinimoExcedidoArgs(value));
+                }
+                else
+                {
+                    _combustible = value;
+                }
+            }
+            get
+            {
+                return _combustible;
+            }
+
+        }
+
+        public int velocidad
+        {
+            get
+            {
+                return _velocidad;
+            }
+            set
+            {
+                if (value >= maxVelocidad)
+                {
+                   // velocidadMaximaExcedida(this, new VelocidadMaximaExcedidaArgs(value));
+                }
+                else
+                {
+                    if (value < 0)
+                    {
+                        _velocidad = 0;
+                    }
+                    else
+                    {
+                        _velocidad = value;
+                    }
+                }
+            }
+        }
+        public void incVelocidad()
+        {
+            try
+            {
+                //Generamos número aleatorio y aumentamos la velocidad
+                velocidad += rand.Next(1, 8);  //Llama a la función de velocidad y la aumenta si corresponde
+                //velocidad
+           }
+            catch (Exception e) {
+
+           }
+        }
+
+        public void incTemperatura()
+        {
+            int tmp;
+            try
+            {
+                //Generamos número aleatorio y llamamos a función para que aumente
+                temperatura += rand.Next(1, 6);
+            }
+            catch(Exception d) {
+            }
+        }
+
+        public void decCombustible()
+        {
+            try
+            {
+                combustible -= rand.Next(1, 6);
+            }
+            catch (Exception d) {
+
+            }
+        }
+
+        public void mover()
+        {
+            if (this.todoOk())
+            {
+                this.decCombustible();
+                this.incTemperatura();
+                this.incVelocidad();
+            }
+        }
+        public bool todoOk()
+        {
+            bool ok = false;
+            if (this.temperatura <= maxTemperatura && this.combustible >= minCombustible)
+            {
+                ok = true;
+            }
+            return ok;
+        }
+        public override string ToString()
+        {
+            string ok = "False";
+            string str = "";
+            if (this.todoOk())
+            {
+                ok = "True";
+            }
+            str = "[" + this.nombre + "] Velocidad: " + this.velocidad + " km/h; Temperatura " +
+                ": " + this.temperatura + "ºC; Combustible: " + this.combustible + " %; Ok: " + ok;
+            return str;
+        }
+    }
+}
